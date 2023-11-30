04450615090fae35eea19c1ea6d2e5501b53c549
pupilRui <142526194+pupilRui@users.noreply.github.com>
2023-11-30 10:00:55 -0800

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StylingAssignment from './StylingAssignment';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <StylingAssignment />
+    </div>
+  );
+}
+
+export default App;
